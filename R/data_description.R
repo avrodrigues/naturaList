c8e7d260a413c3a6a6517cff5d550c2c5f4ace3f
Arthur Rodrigues <rodrigues.arthur.v@gmail.com>
2020-01-31 15:01:11 -0300

    Create data_description.R

---
+++ R/data_description.R
@@ -0,0 +1,18 @@
+#' Occurrence records of Alsophila setosa downloaded from Global Biodiversity
+#' Information Facility (GBIF).
+#'
+#' A dataset contaning 508 occurrence records worldwide. This is the a raw dataset
+#' fom GBIF.
+#'
+#' @format A data frame with 508 rows and 45 variables
+#'
+#' @source GBIF.org (08 July 2019) GBIF Occurrence Download \url{https://doi.org/10.15468/dl.6jesg0}
+"A.setosa"
+
+#' Specialists of ferns and lycophytes of Brazil
+#'
+#' A dataset containing the specialists of ferns and lycophytes of Brazil formated
+#' to be used by \code{naturaList} package
+#'
+#' @format A data frame with 27 rows and 8 columns
+"speciaLists"
