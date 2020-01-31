6baa6634e66ec6e36778a2a5fd2f471e7e83643a
Arthur Rodrigues <rodrigues.arthur.v@gmail.com>
2020-01-31 15:01:14 -0300

    Update classify_occ.R

--- R/classify_occ.R
+++ R/classify_occ.R
@@ -1,35 +1,50 @@
 #' Classify occurrence records in levels of confidence in species determination
 #'
-#' @description Classify occurrence records in levels of confidence in species determination
+#' @description Classify occurrence records in levels of confidence in species
+#' determination
 #'
 #' @param occ dataframe with occurrence records information.
 #' @param spec dataframe with specialists' names. See details.
-#' @param na.rm.coords logical. If TRUE, remove occurrences with NA in latitude or longitude
-#' @param crit.levels character. Vector with levels of confidence in decreasing order.
-#'        The criteria allowed are \code{det_by_spec}, \code{taxonomist},
-#'        \code{image}, \code{sci_colection}, \code{field_obs}, \code{no_criteria_met}. See datails.
-#'
-#'
-#' @param institution.source column of \code{occ} with the name of the institution that provided the data
-#' @param collection.code column of \code{occ} with the codes for institution names
+#' @param na.rm.coords logical. If TRUE, remove occurrences with NA in latitude
+#' or longitude
+#' @param crit.levels character. Vector with levels of confidence in decreasing
+#' order. The criteria allowed are \code{det_by_spec}, \code{taxonomist},
+#' \code{image}, \code{sci_colection}, \code{field_obs}, \code{no_criteria_met}.
+#'  See datails.
+#' @param institution.source column of \code{occ} with the name of the
+#' institution that provided the data
+#' @param collection.code column of \code{occ} with the codes for institution
+#'  names
 #' @param catalog.number column of \code{occ} with catalog number
 #' @param year.event column with of \code{occ} the year of the collection event
-#' @param date.identified column of \code{occ} with the date of species determination
+#' @param date.identified column of \code{occ} with the date of species
+#' determination
 #' @param scientific.name column of \code{occ} with the species names
-#' @param determined.by column of \code{occ} with the name of who determined the species
+#' @param determined.by column of \code{occ} with the name of who determined the
+#'  species
 #' @param longitude column with of \code{occ} longitude in decimal degrees
 #' @param latitude column with of \code{occ} latitude in decimal degrees
-#' @param basis.of.rec column of \code{occ} with the recording types, as in GBIF. See details.
-#' @param media.type column of \code{occ} with the media type of recording. See details.
+#' @param basis.of.rec column of \code{occ} with the recording types, as in GBIF.
+#'  See details.
+#' @param media.type column of \code{occ} with the media type of recording.
+#' See details.
 #' @param occ.id column of \code{occ} with link or code for the occurence record.
 #'
-#' @details Specialist dataframe must have one column for each name and abreviation.
-#' First collumn must have the lastname. Subsequente colummns should have names and abbreviation
+#' @return the \code{occ} data frame plus the classification of each record
+#' in a new column, named \code{naturaList_levels}.
+#'
+#' @details Specialist dataframe must have one column for each name and
+#' abreviation.
+#' First collumn must have the lastname. Subsequente colummns should have names
+#' and abbreviation
 #' for each name. See an exemple with \code{data(speciaList)}
-#' @details \code{basis.of.rec} is a character vector with one of the following types of record:
-#' \code{PRESERVED_SPECIMEN} or \code{HUMAN_OBSERVATION}, as in GBIF data 'basisOfRecord'.
-#' @details media.type uses the same pattern as GBIF data column mediaType indicating the existence
-#' of an associated image by \code{STILLIMAGE}.
+#' @details \code{basis.of.rec} is a character vector with one of the following
+#' types of record:\code{PRESERVED_SPECIMEN} or \code{HUMAN_OBSERVATION}, as in
+#' GBIF data 'basisOfRecord'.
+#'
+#' @details media.type uses the same pattern as GBIF mediaType column,
+#' indicating the existence of an associated image with \code{STILLIMAGE}.
+#'
 #'
 #' @author Arthur V. Rodrigues
 #'
