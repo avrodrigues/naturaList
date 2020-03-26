961622352f69efb70e0234a31448708ebcbc0063
Arthur Rodrigues <rodrigues.arthur.v@gmail.com>
2020-03-26 14:40:46 -0300

    Update get_det_names.R

--- R/get_det_names.R
+++ R/get_det_names.R
@@ -6,9 +6,19 @@
 #' @param occ Data frame with occurrence records information.
 #' @param determined.by Column name of \code{occ} with the name of who determined the
 #'  species.
+#' @param freq Logical. If TRUE output contain the number of times each string is
+#'  repeated in the 'determined.by' column. Default FALSE.
 #'
 #' @export
 
-get_det_names <- function(occ, determined.by = "identifiedBy"){
-  names(table(occ[, determined.by]))
+get_det_names <- function(occ,
+                          determined.by = "identifiedBy",
+                          freq = FALSE){
+    det.string <- names(table(occ[, determined.by]))
+
+  if(freq){
+    det.string <-   table(occ[, determined.by])[order(table(occ[, determined.by]))]
+  }
+
+  return(det.string)
 }
