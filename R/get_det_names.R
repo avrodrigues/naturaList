fdd46328fa87c70ac567ec88a75d9a0de12733b3
Arthur Rodrigues <rodrigues.arthur.v@gmail.com>
2021-03-15 14:01:26 -0300

    update to R CMD CHECK

--- R/get_det_names.R
+++ R/get_det_names.R
@@ -8,19 +8,23 @@
 #'  species.
 #' @param freq Logical. If TRUE output contain the number of times each string is
 #'  repeated in the 'determined.by' column. Default FALSE.
-#' 
-#' @return Character vector containing characters in \code{determined.by} 
+#' @param decreasing logical. sort strings in decreasing order of frequency. Default =
+#'  TRUE
+#'
+#' @return Character vector containing characters in \code{determined.by}
 #'     column of \code{occ}.
-#' 
+#'
 #' @export
 
 get_det_names <- function(occ,
                           determined.by = "identifiedBy",
-                          freq = FALSE){
+                          freq = FALSE,
+                          decreasing = TRUE){
     det.string <- names(table(occ[, determined.by]))
 
   if(freq){
-    det.string <-   table(occ[, determined.by])[order(table(occ[, determined.by]))]
+    det.string <-   table(occ[, determined.by])[order(table(occ[, determined.by]),
+                                                      decreasing = decreasing)]
   }
 
   return(det.string)
